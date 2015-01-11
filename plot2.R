cec6ba3d5874f17d327f9b851a4c79a7b18823cd
Xiao <caramel-slice@hotmail.com>
2015-01-11 17:26:13 -0500

    test1
    /Users/caramel-slice/Desktop/Project1

---
+++ plot2.R
@@ -0,0 +1,6 @@
+# plot2.R
+newdata <- subset(read.table("household_power_consumption.txt", header = TRUE, sep =";"), Date =="1/2/2007" | Date =="2/2/2007", select=Date:Sub_metering_3)
+quartz()
+plot(as.numeric(newdata$Global_active_power), type = "l", ylab="Global Active Power (kilowatts)")
+dev.copy(png, file = "plot2.png", width = 480, height = 480, units = "px")
+dev.off()
